6c00afd751b4cc155bbd2ff70489b9be3e3ce989
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2023-07-19 01:55:58 -0400

    moved vintagetextbox import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useMediaQuery } from 'react-responsive'
 import MobileHomePage from "./MobileHomePage";
 import DesktopHomePage from "./DesktopHomePage";
-import VintageTextBox from "../components/VintageTextbox"
 
 const Home = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 }); 

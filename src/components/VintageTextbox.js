5c7fe2ea8dfb5de9329a7d28f802f31ca8947502
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2023-07-23 17:58:44 -0400

    add reactmarkdown, add philosophyText API call

--- src/components/VintageTextbox.js
+++ src/components/VintageTextbox.js
@@ -1,34 +1,44 @@
-import React, { useEffect } from "react";
-import GetText from "../functions/GetText"
-import "../styles/VintageTextbox.css"
-import okEmoji from "../assets/ok-button.png"
+import React, { useEffect, useState } from "react";
+import "../styles/VintageTextbox.css";
+import okEmoji from "../assets/ok-button.png";
+import axios from "axios";
 
 const VintageTextbox = () => {
+  const [philosophyText, setPhilosophyText] = useState("");
 
-    const apiUrl="http://localhost:1337/api/philosophy-text"
+  useEffect(() => {
+    const apiUrl = "https://strapi-production-5302.up.railway.app/api/philosophy-text";
 
-    useEffect(() => {
-        window.scrollTo(0, 0); // Scroll to the top of the item
-      }, []);
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        // handle API response data
+        const philosophyTextData = response.data.data.attributes.text;
+        setPhilosophyText(philosophyTextData);
+      })
+      .catch((error) => {
+        console.error("Error fetching philosophy text:", error);
+      });
+  }, []);
 
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the item
+  }, []);
 
-    return (
-
-
-        <div className="philosophy-element">
-            <div className="vintage-textbox">
-                <div className="philosophy-title">
-                <p className="philosophy-title play-font">philosophy</p>
-                <button>X</button>
-                </div>
-                <div className="textbox-body">
-                <GetText apiUrl={apiUrl} />
-                </div>
-                <button className="ok-button"><img className="ok-emoji" src={ okEmoji } alt="ok emoji" /></button>
-            </div>
+  return (
+    <div className="philosophy-element">
+      <div className="vintage-textbox">
+        <div className="philosophy-title">
+          <p className="philosophy-title play-font">philosophy</p>
+          <button>X</button>
         </div>
-
-    );
-}
+        <div className="textbox-body">{philosophyText}</div>
+        <button className="ok-button">
+          <img className="ok-emoji" src={okEmoji} alt="ok emoji" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default VintageTextbox;

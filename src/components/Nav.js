8a8f4c538cb349d9a82f5445cdbdf060593f6c02
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2023-07-24 03:19:38 -0400

    fix logo loading

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,37 +2,40 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import pinkTrack from '../assets/pink divider.svg';
 import '../styles/Nav.css';
-import NavigationMenu from '../components/NavigationMenu';
-import GetLogo from "../functions/GetLogo";
+import NavigationMenu from '../components/NavigationMenu'
+import GetLogo from "../functions/GetLogo"
 
 function Nav() {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
 
+
   return (
-    <div className={`nav-container nav-container${isHomePage ? '-home' : ''}`}>
-      <Link className={`desktop-logo${isHomePage ? '-home' : ''}`} to="/">
-        <GetLogo logoType={`${isHomePage ? 'desktopLogoHome' : 'desktopLogo'}`} loading="lazy" />
-      </Link>
-      <div className="navigation-menu">
-        <NavigationMenu />
-      </div>
-      <div className={`pink-track ${!isHomePage ? 'hidden' : ''}`}>
-        <img src={pinkTrack} alt="pink divider line" id="pink-track" />
-      </div>
-      <div className={`contact-info-container ${!isHomePage ? 'hidden' : ''}`}>
-        <Link className="contact-info trispace-font contact-info-desktop" to="mailto:internDAO@gmail.com">
-          <span role="img" aria-label="inbox tray" className="emoji-bullet">&#128229;</span>
-          <h5 className="contact-info-text trispace-font">internDAO@gmail.com</h5>
+    <div className="nav-container">
+        <Link className={`desktop-logo ${isHomePage ? 'visible' : 'hidden'}`} to="/">
+          <GetLogo logoType="desktopLogo" />
+          <GetLogo logoType="desktopLogoText" />
         </Link>
-        <div className="contact-info contact-info-desktop">
-          <span role="img" aria-label="penguin emoji" className="emoji-bullet">&#128039;</span>
-          <h5 className="contact-info-text trispace-font">carrier-pigeon #4883</h5>
+        <div className="navigation-menu">
+        <NavigationMenu />
         </div>
-      </div>
+        <div className={`pink-track ${!isHomePage ? 'hidden' : ''}`}>
+          <img src={pinkTrack} alt="pink divider line" id="pink-track" />
+        </div>
+        <div className={`contact-info-container ${!isHomePage ? 'hidden' : ''}`}>
+          <Link className="contact-info trispace-font contact-info-desktop" to="mailto:internDAO@gmail.com">
+            <span role="img" aria-label="inbox tray" className="emoji-bullet">&#128229;</span>
+            <h5 className="contact-info-text trispace-font">internDAO@gmail.com</h5>
+          </Link>
+          <div className="contact-info contact-info-desktop">
+            <span role="img" aria-label="penguin emoji" className="emoji-bullet">&#128039;</span>
+            <h5 className="contact-info-text trispace-font">carrier-pigeon #4883</h5>
+          </div>
+        </div>
+
       <div className="blank"></div>
     </div>
   );
 }
 
-export default Nav;
+export default Nav
--- src/functions/GetLogo.js
+++ src/functions/GetLogo.js
@@ -1,34 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function GetLogo({ logoType }) {
+function GetLogo({ logoType = 'desktopLogo' }) {
   const [logoData, setLogoData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true); // Add a loading state
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get("https://strapi-production-5302.up.railway.app/api/logo?populate=*")
+      .get('https://strapi-production-5302.up.railway.app/api/logo?populate=*')
       .then((response) => {
         if (response.data.data.attributes[logoType]) {
-          setLogoData(response.data.data.attributes[logoType].data.attributes.url);
+          const imageUrl = response.data.data.attributes[logoType].data.attributes.url;
+          // Add a cache-busting query parameter using a random number
+          const cacheBustingUrl = `${imageUrl}?cache=${Math.random()}`;
+          setLogoData(cacheBustingUrl);
         } else {
-          console.error("Invalid logoType:", logoType);
+          console.error('Invalid logoType:', logoType);
           // Handle the error, set a default logo, or show an error message
         }
-        setIsLoading(false); // Set loading to false after data is fetched
+        setIsLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching logo data:", error);
-        setIsLoading(false); // Set loading to false even if there's an error
+        console.error('Error fetching logo data:', error);
+        setIsLoading(false);
       });
   }, [logoType]);
 
   if (isLoading) {
     // Show a loading indicator or default logo while waiting for data
-    return <div>Loading logo...</div>;
+    return <></>;
   }
 
-  return <img src={logoData} alt="Logo" className={logoType} />;
+  return <img src={logoData} alt="Logo" className={logoType} loading="lazy" />;
 }
 
-export default GetLogo
+export default GetLogo;

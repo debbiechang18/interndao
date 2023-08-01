6bb56abfa3cbd7898470809a06da8979eec6f89e
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2023-08-01 15:02:14 -0400

    adjust vintage textbox

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Research.css";
+import CardList from '../components/CardList';
+import axios from "axios";
 
 const Research = () => {
-  return (
-    <h1>research</h1>
-  ) 
-}
+  const itemsPerPage = 8; // Number of research cards to display per page
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default Research
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    const apiUrl = "https://strapi-production-5302.up.railway.app/api/philosophy-text";
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        // handle API response data
+        const philosophyTextData = response.data.data.attributes.text;
+        setPhilosophyText(philosophyTextData);
+      })
+      .catch((error) => {
+        console.error("Error fetching philosophy text:", error);
+      });
+  }, []);
+
+
+  return (
+    <div>
+      <CardList data={researchData} itemsPerPage={itemsPerPage} />
+    </div>
+  );
+};
+
+export default Research;

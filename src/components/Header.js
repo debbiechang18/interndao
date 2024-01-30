c8edcab652b1b960725c379a1928343aaeb4ce13
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2024-01-30 13:23:28 -0500

    Update default logo if API call fails

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
 import "../styles/Header.css";
 import hamburgerIcon from "../assets/burger.png";
-import GetLogo from "../functions/GetLogo"
+import GetLogo from "../functions/GetLogo";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,12 +14,18 @@ const Header = () => {
 
   return (
     <div className="header-container">
-      <Link className="logo-link" to="/"><GetLogo logoType="mobileLogo" alt="logo" /></Link>
+      <Link className="logo-link" to="/">
+        <GetLogo logoType="mobileLogo" alt="logo" />
+      </Link>
       <span
         className={`hamburger-icon ${menuOpen ? "menu-open" : ""}`}
         onClick={toggleMenu}
       >
-        <img className="hamburger-icon-img" src={hamburgerIcon} alt="hamburger icon" />
+        <img
+          className="hamburger-icon-img"
+          src={hamburgerIcon}
+          alt="hamburger icon"
+        />
       </span>
       <HamburgerMenu isOpen={menuOpen} onCloseMenu={() => setMenuOpen(false)} />
     </div>
--- src/functions/GetLogo.js
+++ src/functions/GetLogo.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function GetLogo({ logoType}) {
-  const [logoData, setLogoData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+function GetLogo({ logoType }) {
+  const defaultLogo = "logo-icon-yellow.svg";
+  const defaultLogoText = "logo-text.svg";
+
+  // Determine the default logo based on the logoType
+  const defaultLogoData =
+    logoType === "desktopLogo"
+      ? defaultLogo
+      : logoType === "desktopLogoText"
+      ? defaultLogoText
+      : null;
+
+  const [logoData, setLogoData] = useState(defaultLogoData);
 
   useEffect(() => {
     axios
-      .get('https://strapi-production-5302.up.railway.app/api/logo?populate=*')
+      .get("https://strapi-production-5302.up.railway.app/api/logo?populate=*")
       .then((response) => {
         if (response.data.data.attributes[logoType]) {
-          const imageUrl = response.data.data.attributes[logoType].data.attributes.url;
-          // Add a cache-busting query parameter using a random number
-          const cacheBustingUrl = `${imageUrl}?cache=${Math.random()}`;
-          setLogoData(cacheBustingUrl);
-        } else {
-          console.error('Invalid logoType:', logoType);
-          // Handle the error, set a default logo, or show an error message
+          const imageUrl =
+            response.data.data.attributes[logoType].data.attributes.url;
+          setLogoData(`${imageUrl}?cache=${Math.random()}`);
         }
-        setIsLoading(false);
       })
       .catch((error) => {
-        console.error('Error fetching logo data:', error);
-        setIsLoading(false);
+        console.error("Error fetching logo data:", error);
+        // In case of error, logoData remains the default
       });
   }, [logoType]);
-
-  if (isLoading) {
-    // Show a loading indicator or default logo while waiting for data
-    return <></>;
-  }
 
   return <img src={logoData} alt="Logo" className={logoType} loading="lazy" />;
 }
